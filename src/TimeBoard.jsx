2c43255f2e7602a018c411de821bd621a8c52cb5
ProdigiousPerson <54821554+ADoofus@users.noreply.github.com>
2023-04-07 12:19:34 -0700

    Update TimeBoard.jsx

--- src/TimeBoard.jsx
+++ src/TimeBoard.jsx
@@ -42,7 +42,7 @@ function calculateBestTime() {
     let newTime = 0;
 
     //Minutes
-    if (time.contains(':')) {
+    if (time.includes(':')) {
       minutes = time.split(':', 1)[0].slice(0,-1).parseInt() * 60;
       newTime += minutes
       time.replace(minutes.toString() + ":",'')
